066d3d74b4de0be2252788485063615ed187a5ea
kienmai <maingockien01@gmail.com>
2024-01-18 15:59:50 -0600

    TEAM8-1: set up nestjs serve react and rest-api

--- apps/backend/src/app.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { APPS_NAME } from '@team8/constants/apps';
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return `Hello ${APPS_NAME}!`;
-  }
-}
---
+++ apps/backend/src/routes.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { HelloModule } from './hello/hello.module';
+import { RouterModule } from '@nestjs/core';
+
+@Module({
+  imports: [
+    HelloModule,
+    RouterModule.register([
+      {
+        path: '/rest-api',
+        children: [
+          {
+            path: '/hello',
+            module: HelloModule,
+          },
+        ],
+      },
+    ]),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class RoutesModule {}
