464bef297ae79f1163007092cf9e07420a47a3f7
kienmai <maingockien01@gmail.com>
2024-01-18 14:15:47 -0600

    TEAM8-1: Create apps constant for sharing

--- apps/backend/src/app.service.ts
+++ apps/backend/src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { APPS_NAME } from '@team8/constants/apps';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `Hello ${APPS_NAME}!`;
   }
 }
