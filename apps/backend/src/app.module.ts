a3657a15328d96160230a780699a97ef87fe4232
kienmai <maingockien01@gmail.com>
2024-02-01 15:14:05 -0600

    TEAM8-1: connect nestjs to database

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,14 +2,25 @@ import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { RoutesModule } from './routes.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
 	imports: [
 		ServeStaticModule.forRoot({
-			rootPath: join(__dirname, '/../../../frontend/build'),
+			rootPath: join(__dirname, '/../../../frontend/dist'),
 			exclude: ['/rest-api*'],
 		}),
 		RoutesModule,
+		TypeOrmModule.forRoot({
+			type: 'mysql',
+			host: 'database',
+			port: 3306,
+			username: 'root',
+			password: 'root',
+			database: 'test',
+			autoLoadEntities: true,
+			synchronize: true,
+		}),
 	],
 	controllers: [],
 	providers: [],
