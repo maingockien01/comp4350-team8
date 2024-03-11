5e20f34667a3e7484edceabfc3870bd5dc0df0c7
Quang Anh <lqa.uom.cs@gmail.com>
2024-03-10 22:45:13 -0500

    Fix unit tests

--- apps/backend/test/unit/degrees/degree.controller.unit.ts
+++ apps/backend/test/unit/degrees/degree.controller.unit.ts
@@ -11,88 +11,102 @@ import { NotFoundException } from '@nestjs/common';
 const moduleMocker = new ModuleMocker(global);
 
 describe('DegreeController', () => {
-    let degreeController: DegreeController;
-    let degreeService: DegreeService;
+	let degreeController: DegreeController;
+	let degreeService: DegreeService;
 
-    beforeEach(async () => {
-        const moduleRef = await Test.createTestingModule({
-            controllers: [DegreeController],
-            providers: [DegreeService,{
-                provide: getRepositoryToken(Degree),
-                useClass: Repository
-            }]
-        }).compile();
+	beforeEach(async () => {
+		const moduleRef = await Test.createTestingModule({
+			controllers: [DegreeController],
+			providers: [
+				DegreeService,
+				{
+					provide: getRepositoryToken(Degree),
+					useClass: Repository,
+				},
+			],
+		}).compile();
 
-        degreeController = moduleRef.get<DegreeController>(DegreeController);
-        degreeService = moduleRef.get<DegreeService>(DegreeService);
-    });
-    
-    describe('findAll', () => {
-        it('should return an array of degrees', async () => {
-        const result: DegreeDTO[] = [
-            {
-                "did": 1,
-                "name": "Computer Science"
-            },
-        ];
+		degreeController = moduleRef.get<DegreeController>(DegreeController);
+		degreeService = moduleRef.get<DegreeService>(DegreeService);
+	});
 
-        jest.spyOn(degreeService, 'findAll').mockImplementation(() => Promise.resolve(result));
-        expect(await degreeController.findAll()).toBe(result);
-        });
-    });
+	describe('findAll', () => {
+		it('should return an array of degrees', async () => {
+			const result: DegreeDTO[] = [
+				{
+					did: 1,
+					name: 'Computer Science',
+				},
+			];
 
-    describe('getDegreeWithRoadmap', () => {
-        it('should return a degree with roadmap', async () => {
-            const result: DegreeDTO[] = [{
-                did: 1,
-                name: "Computer Science",
-                recommendedCourses: [{
-                    cid: 1,
-                    courseName: "Software Engineering",
-                    description: "Learn how to develop software",
-                    courseNumber: 4350,
-                    department: "Computer Science",
-                    prerequisites: []
-                }]
-            }];
+			jest.spyOn(degreeService, 'findAll').mockImplementation(() => Promise.resolve(result));
+			expect(await degreeController.findAll()).toBe(result);
+		});
+	});
 
-            jest.spyOn(degreeService, 'getDegrees').mockImplementation((criterias: Partial<Degree>, withRecommendedCourses: boolean) => {
+	describe('getDegreeWithRoadmap', () => {
+		it('should return a degree with roadmap', async () => {
+			const result: DegreeDTO[] = [
+				{
+					did: 1,
+					name: 'Computer Science',
+					recommendedCourses: [
+						{
+							cid: 1,
+							courseName: 'Software Engineering',
+							description: 'Learn how to develop software',
+							courseNumber: 'COMP 4350',
+							department: 'Computer Science',
+							prerequisites: [],
+						},
+					],
+				},
+			];
 
-                expect(criterias).toEqual({ did: 1 });
-                expect(withRecommendedCourses).toBeTruthy();
+			jest.spyOn(degreeService, 'getDegrees').mockImplementation(
+				(criterias: Partial<Degree>, withRecommendedCourses: boolean) => {
+					expect(criterias).toEqual({ did: 1 });
+					expect(withRecommendedCourses).toBeTruthy();
 
-                return Promise.resolve(result);
-            });
-            expect(await degreeController.getDegreeById(1, true)).toBe(result[0]);
-        });
+					return Promise.resolve(result);
+				},
+			);
+			expect(await degreeController.getDegreeById(1, true)).toBe(result[0]);
+		});
 
-        it('should return a degree without roadmap', async () => {
-            const result: DegreeDTO[] = [{
-                did: 1,
-                name: "Computer Science",
-            }];
+		it('should return a degree without roadmap', async () => {
+			const result: DegreeDTO[] = [
+				{
+					did: 1,
+					name: 'Computer Science',
+				},
+			];
 
-            jest.spyOn(degreeService, 'getDegrees').mockImplementation((criterias: Partial<Degree>, withRecommendedCourses: boolean) => {
+			jest.spyOn(degreeService, 'getDegrees').mockImplementation(
+				(criterias: Partial<Degree>, withRecommendedCourses: boolean) => {
+					expect(criterias).toEqual({ did: 1 });
+					expect(withRecommendedCourses).toBeFalsy();
 
-                expect(criterias).toEqual({ did: 1 });
-                expect(withRecommendedCourses).toBeFalsy();
+					return Promise.resolve(result);
+				},
+			);
+			expect(await degreeController.getDegreeById(1, false)).toBe(result[0]);
+		});
 
-                return Promise.resolve(result);
-            });
-            expect(await degreeController.getDegreeById(1, false)).toBe(result[0]);
-        });
+		it('should return not found', async () => {
+			const result: DegreeDTO[] = [];
 
-        it('should return not found', async () => {
-            const result: DegreeDTO[] = [];
+			jest.spyOn(degreeService, 'getDegrees').mockImplementation(
+				(criterias: Partial<Degree>, withRecommendedCourses: boolean) => {
+					expect(criterias).toEqual({ did: 1 });
+					expect(withRecommendedCourses).toBeFalsy();
 
-            jest.spyOn(degreeService, 'getDegrees').mockImplementation((criterias: Partial<Degree>, withRecommendedCourses: boolean) => {
-
-                expect(criterias).toEqual({ did: 1 });
-                expect(withRecommendedCourses).toBeFalsy();
-
-                return Promise.resolve(result);
-            });
-            await expect(() => degreeController.getDegreeById(1, false)).rejects.toThrow(new NotFoundException(`Degree with id 1 not found`));
-        });
-    });
+					return Promise.resolve(result);
+				},
+			);
+			await expect(() => degreeController.getDegreeById(1, false)).rejects.toThrow(
+				new NotFoundException(`Degree with id 1 not found`),
+			);
+		});
+	});
 });
