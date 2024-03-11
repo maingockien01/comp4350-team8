732797a0665c42fa0bffec58d8373483b2b9a9ef
kienmai <maingockien01@gmail.com>
2024-03-11 18:50:53 -0500

    Issue 8: add savePersonalRoadmap functionality

--- apps/backend/src/roadmap/personal.roadmap.service.ts
+++ apps/backend/src/roadmap/personal.roadmap.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@nestjs/common';
-import { Roadmap } from '@team8/types/domain/roadmap.model';
+import { InvalidRoadmapException, Roadmap } from '@team8/types/domain/roadmap.model';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../entities/user.entity';
-import { Repository } from 'typeorm';
+import { In, Repository } from 'typeorm';
+import { Course } from '../entities/course.entity';
 
 @Injectable()
 export class PersonalRoadmapService {
 	constructor(
 		@InjectRepository(User)
 		private readonly userRepository: Repository<User>,
+		@InjectRepository(Course)
+		private readonly courseRepository: Repository<Course>,
 	) {}
 	async getPersonalRoadmap(userId: number): Promise<Roadmap> {
 		const user = await this.userRepository.findOneOrFail({
@@ -23,7 +26,33 @@ export class PersonalRoadmapService {
 		return new Roadmap(user.plannedCourses);
 	}
 
-	savePersonalRoadmap(userId: number): Roadmap {
-		return null;
+	async savePersonalRoadmap(userId: number, cids: number[]): Promise<Roadmap> {
+		const courses = await this.courseRepository.find({
+			where: {
+				cid: In(cids),
+			},
+		});
+
+		if (courses.length !== cids.length) {
+			throw new Error('Some courses do not exist');
+		}
+
+		const roadmap = new Roadmap(courses);
+
+		if (!roadmap.isValid()) {
+			throw new InvalidRoadmapException();
+		}
+
+		const user = await this.userRepository.findOneOrFail({
+			where: {
+				uid: userId,
+			},
+		});
+
+		user.plannedCourses = courses;
+
+		await this.userRepository.save(user);
+
+		return roadmap;
 	}
 }
--- apps/backend/test/e2e/roadmap/personal.roadmap.service.e2e.ts
+++ apps/backend/test/e2e/roadmap/personal.roadmap.service.e2e.ts
@@ -38,4 +38,38 @@ describe('PersonalRoadmapService', () => {
 			expect(roadmap.recommendedCourses).toHaveLength(courses.length);
 		});
 	});
+
+	describe('savePersonalRoadmap', () => {
+		it('should throw for non-existing user', async () => {
+			await expect(personalRoadmapService.savePersonalRoadmap(0, [])).rejects.toThrow();
+		});
+
+		it('should throw for non-existing courses', async () => {
+			const user = await createUser(app);
+			await expect(
+				personalRoadmapService.savePersonalRoadmap(user.uid, [Number.MAX_SAFE_INTEGER]),
+			).rejects.toThrow();
+		});
+
+		it('should throw for invalid roadmap', async () => {
+			const user = await createUser(app);
+			const course = await createCourse(app);
+			const anotherCourse = await createCourse(app, { prerequisites: [course] });
+			await expect(
+				personalRoadmapService.savePersonalRoadmap(user.uid, [anotherCourse.cid]),
+			).rejects.toThrow();
+		});
+
+		it('should save roadmap for user', async () => {
+			const courses = await Promise.all(Array(10).map(() => createCourse(app)));
+			const user = await createUser(app);
+			await personalRoadmapService.savePersonalRoadmap(
+				user.uid,
+				courses.map((course) => course.cid),
+			);
+
+			const roadmap = await personalRoadmapService.getPersonalRoadmap(user.uid);
+			expect(roadmap.recommendedCourses).toHaveLength(courses.length);
+		});
+	});
 });
