029098f2e3316c990e8b5f36682183aadcf133a5
kienmai <maingockien01@gmail.com>
2024-03-10 23:10:24 -0500

    Issue 8: make e2e test run

--- apps/backend/test/e2e/roadmap/personal.roadmap.service.e2e.ts
+++ apps/backend/test/e2e/roadmap/personal.roadmap.service.e2e.ts
@@ -1,12 +1,13 @@
 import { INestApplication } from '@nestjs/common';
 import { PersonalRoadmapService } from '../../../src/roadmap/personal.roadmap.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from '../../../src/entities/user.entity';
 
 const app: INestApplication = global.app;
 
 describe('PersonalRoadmapService', () => {
-	const service = app.get<PersonalRoadmapService>(PersonalRoadmapService);
-
 	it('should be defined', () => {
-		expect(service).toBeDefined();
+		expect(app.get(getRepositoryToken(User))).toBeDefined();
+		expect(app.get(PersonalRoadmapService)).toBeDefined();
 	});
 });
