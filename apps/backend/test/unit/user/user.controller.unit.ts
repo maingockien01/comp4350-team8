1c94b92e06401dc524bb7f916f081e856f5e43c3
Quang Anh <lqa.uom.cs@gmail.com>
2024-03-12 17:26:39 -0500

    Issue#74: Fix user.controller unit test

--- apps/backend/test/unit/user/user.controller.unit.ts
+++ apps/backend/test/unit/user/user.controller.unit.ts
@@ -6,44 +6,71 @@ import { Repository } from 'typeorm';
 import { User } from '../../../src/entities/user.entity';
 import { SectionDTO } from 'packages/types/dtos/section/section.dto';
 import { Section } from '../../../src/entities/section.entity';
+
 describe('UserController', () => {
-    let userController: UserCourseController;
-    let userService:UserCourseService;
+	let userController: UserCourseController;
+	let userService: UserCourseService;
 
-    beforeEach(async () => {
-        const moduleRef = await Test.createTestingModule({
-            controllers: [UserCourseController],
-            providers:[UserCourseService,{
-                provide: getRepositoryToken(User),
-                useClass: Repository
-            }],
-        }).compile();
+	beforeEach(async () => {
+		const moduleRef = await Test.createTestingModule({
+			controllers: [UserCourseController],
+			providers: [
+				UserCourseService,
+				{
+					provide: getRepositoryToken(User),
+					useClass: Repository,
+				},
+			],
+		}).compile();
 
-        userController = moduleRef.get<UserCourseController>(UserCourseController);
-        userService = moduleRef.get<UserCourseService>(UserCourseService);
-    });
-    
-    describe('find', () => {
-        it('should return a User with matched uid', async () => {
-        const result = {"uid":1,"fullName":"Jack Nguyen","username":"jacknguyen","hashPassword":"jack123","pictureProfile":"default"}
-        jest.spyOn(userService, 'find').mockImplementation(() => Promise.resolve(result));
-        expect(await userController.findOne(1)).toBe(result);
-        });
-    });
+		userController = moduleRef.get<UserCourseController>(UserCourseController);
+		userService = moduleRef.get<UserCourseService>(UserCourseService);
+	});
 
-    describe('find', () => {
-        it('should return an array of Courses with time and building with matched uid and tid', async () => {
-        const result = [{"courseName":"COMP 702","time":"11:00 AM","location":"T501"},{"courseName":"COMP 1202","time":"11:00 AM","location":"D401"}]
-        jest.spyOn(userService, 'findActive').mockImplementation(() => Promise.resolve(result));
-        expect(await userController.findActive(1,10)).toBe(result);
-        });
-    });
+	describe('find', () => {
+		it('should return a User with matched uid', async () => {
+			const result = {
+				uid: 1,
+				fullName: 'Jack Nguyen',
+				username: 'jacknguyen',
+				hashPassword: 'jack123',
+				pictureProfile: 'default',
+			};
+			jest.spyOn(userService, 'find').mockImplementation(() => Promise.resolve(result));
+			expect(await userController.findOne(1)).toBe(result);
+		});
+	});
 
-    describe('findAll', () => {
-        it('should return an array of all Users', async () => {
-        const result = [{"uid":1,"fullName":"Jack Nguyen","username":"jacknguyen","hashPassword":"jack123","pictureProfile":"default"}]
-        jest.spyOn(userService, 'findAll').mockImplementation(() => Promise.resolve(result));
-        expect(await userController.findAll()).toBe(result);
-        });
-    });
+	describe('find', () => {
+		it('should return an array of Courses with time and building with matched uid and tid', async () => {
+			const result = [
+				{ courseName: 'COMP 702', time: '11:00 AM', location: 'T501' },
+				{ courseName: 'COMP 1202', time: '11:00 AM', location: 'D401' },
+			];
+			jest.spyOn(userService, 'findActive').mockImplementation(() => Promise.resolve(result));
+			expect(
+				await userController.findActive(1, 10, {
+					user: {
+						uid: 1,
+					},
+				}),
+			).toBe(result);
+		});
+	});
+
+	describe('findAll', () => {
+		it('should return an array of all Users', async () => {
+			const result = [
+				{
+					uid: 1,
+					fullName: 'Jack Nguyen',
+					username: 'jacknguyen',
+					hashPassword: 'jack123',
+					pictureProfile: 'default',
+				},
+			];
+			jest.spyOn(userService, 'findAll').mockImplementation(() => Promise.resolve(result));
+			expect(await userController.findAll()).toBe(result);
+		});
+	});
 });
