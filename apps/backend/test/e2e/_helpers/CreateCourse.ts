12a848230cfb14884a35fae40accfe0016b3c077
kienmai <maingockien01@gmail.com>
2024-03-11 19:43:29 -0500

    Issue 8: fix createCourse util function

--- apps/backend/test/e2e/_helpers/CreateCourse.ts
+++ apps/backend/test/e2e/_helpers/CreateCourse.ts
@@ -2,9 +2,10 @@ import { INestApplication } from '@nestjs/common';
 import { randomStringGenerator } from '@nestjs/common/utils/random-string-generator.util';
 import { Course } from '../../../src/entities/course.entity';
 import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 export function createCourse(app: INestApplication, overrides: Partial<Course> = {}): Promise<Course> {
-	return app.get<Repository<Course>>('CourseRepository').save({
+	return app.get<Repository<Course>>(getRepositoryToken(Course)).save({
 		title: randomStringGenerator(),
 		description: randomStringGenerator(),
 		...overrides,
