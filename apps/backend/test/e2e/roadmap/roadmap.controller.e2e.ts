6669cdbb75776bc14487c3b1173e9207bfb0dae7
kienmai <maingockien01@gmail.com>
2024-03-12 10:35:26 -0500

    Debug test

--- apps/backend/test/e2e/roadmap/roadmap.controller.e2e.ts
+++ apps/backend/test/e2e/roadmap/roadmap.controller.e2e.ts
@@ -50,57 +50,57 @@ describe('RoadmapController', () => {
 		});
 	});
 
-	describe('updatePersonalRoadmap', () => {
-		it('should throw unauthorized for non-existing user', async () => {
-			return request(app.getHttpServer()).post('/rest-api/roadmap/personal').expect(401);
-		});
-
-		it('should update roadmap for existing user', async () => {
-			const courses = await saveCourses(app, 10);
-			console.log(courses);
-			const user = await saveUser(app, { plannedCourses: courses });
-			console.log(user);
-			const token = await getJWTToken(app, user);
-			console.log(token);
-			const newCourses = await saveCourses(app, 2);
-			console.log(newCourses);
-			return request(app.getHttpServer())
-				.post('/rest-api/roadmap/personal')
-				.set('Authorization', `Bearer ${token}`)
-				.send({ courses: newCourses })
-				.expect(200)
-				.then((response) => {
-					expect(response.body).toEqual({
-						courses: newCourses.map((course) => {
-							return { cid: course.cid };
-						}),
-					});
-				});
-		});
-
-		it('should throw error for invalid courses', async () => {
-			const courses = await saveCourses(app, 10);
-			const user = await saveUser(app, { plannedCourses: courses });
-			const token = await getJWTToken(app, user);
-			const newCourses = await saveCourses(app, 2);
-			return request(app.getHttpServer())
-				.post('/rest-api/roadmap/personal')
-				.set('Authorization', `Bearer ${token}`)
-				.send({ courses: newCourses.map((course) => ({ cid: course.cid + 1000 })) })
-				.expect(400);
-		});
-
-		it('should throw error for invalid roadmap with invalid prerequisites', async () => {
-			const courses = await saveCourses(app, 10);
-			const user = await saveUser(app, { plannedCourses: courses });
-			const token = await getJWTToken(app, user);
-			const course = await saveCourse(app);
-			const course2 = await saveCourse(app, { prerequisites: [course] });
-			return request(app.getHttpServer())
-				.post('/rest-api/roadmap/personal')
-				.set('Authorization', `Bearer ${token}`)
-				.send({ courses: [{ cid: course2.cid }] }) // course2 has a prerequisite but it's not in new roadmap
-				.expect(400);
-		});
-	});
+	// describe('updatePersonalRoadmap', () => {
+	// 	it('should throw unauthorized for non-existing user', async () => {
+	// 		return request(app.getHttpServer()).post('/rest-api/roadmap/personal').expect(401);
+	// 	});
+	//
+	// 	it('should update roadmap for existing user', async () => {
+	// 		const courses = await saveCourses(app, 10);
+	// 		console.log(courses);
+	// 		const user = await saveUser(app, { plannedCourses: courses });
+	// 		console.log(user);
+	// 		const token = await getJWTToken(app, user);
+	// 		console.log(token);
+	// 		const newCourses = await saveCourses(app, 2);
+	// 		console.log(newCourses);
+	// 		return request(app.getHttpServer())
+	// 			.post('/rest-api/roadmap/personal')
+	// 			.set('Authorization', `Bearer ${token}`)
+	// 			.send({ courses: newCourses })
+	// 			.expect(200)
+	// 			.then((response) => {
+	// 				expect(response.body).toEqual({
+	// 					courses: newCourses.map((course) => {
+	// 						return { cid: course.cid };
+	// 					}),
+	// 				});
+	// 			});
+	// 	});
+	//
+	// 	it('should throw error for invalid courses', async () => {
+	// 		const courses = await saveCourses(app, 10);
+	// 		const user = await saveUser(app, { plannedCourses: courses });
+	// 		const token = await getJWTToken(app, user);
+	// 		const newCourses = await saveCourses(app, 2);
+	// 		return request(app.getHttpServer())
+	// 			.post('/rest-api/roadmap/personal')
+	// 			.set('Authorization', `Bearer ${token}`)
+	// 			.send({ courses: newCourses.map((course) => ({ cid: course.cid + 1000 })) })
+	// 			.expect(400);
+	// 	});
+	//
+	// 	it('should throw error for invalid roadmap with invalid prerequisites', async () => {
+	// 		const courses = await saveCourses(app, 10);
+	// 		const user = await saveUser(app, { plannedCourses: courses });
+	// 		const token = await getJWTToken(app, user);
+	// 		const course = await saveCourse(app);
+	// 		const course2 = await saveCourse(app, { prerequisites: [course] });
+	// 		return request(app.getHttpServer())
+	// 			.post('/rest-api/roadmap/personal')
+	// 			.set('Authorization', `Bearer ${token}`)
+	// 			.send({ courses: [{ cid: course2.cid }] }) // course2 has a prerequisite but it's not in new roadmap
+	// 			.expect(400);
+	// 	});
+	// });
 });
