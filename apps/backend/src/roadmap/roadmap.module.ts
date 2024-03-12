4198e4ff25b798879a069fe75b5d27dd2ab474b3
kienmai <maingockien01@gmail.com>
2024-03-11 19:32:05 -0500

    Issue 8: add course repo to roadmap module

--- apps/backend/src/roadmap/roadmap.module.ts
+++ apps/backend/src/roadmap/roadmap.module.ts
@@ -3,9 +3,10 @@ import { RoadmapController } from './roadmap.controller';
 import { PersonalRoadmapService } from './personal.roadmap.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../entities/user.entity';
+import { Course } from '../entities/course.entity';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([User])],
+	imports: [TypeOrmModule.forFeature([User, Course])],
 	controllers: [RoadmapController],
 	providers: [PersonalRoadmapService],
 	exports: [],
