39b2d88c28c1611774738cd5fd1b6de3f3691c48
Kien Mai <maingockien01@gmail.com>
2024-03-16 21:58:10 -0500

    Fixing tests

--- apps/backend/test/unit/degrees/degree.controller.unit.ts
+++ apps/backend/test/unit/degrees/degree.controller.unit.ts
@@ -73,22 +73,7 @@ describe('DegreeController', () => {
 								name: 'Computer Science',
 								abbreviation: 'COMP'
 							},
-							sections: [{
-								sid: 1,
-								term: {
-									tid: 1,
-									year: 2021,
-									season: 'Winter',
-								},
-								location: {
-									lid: 1,
-									building: 'T',
-									roomNumber: 501,
-								},
-								time: '11:00 AM',
-								sectionName: 'A01',
-								professor: 'Dr. Smith',
-							}],
+							sections: [],
 							prerequisites: [],
 						},
 					],
