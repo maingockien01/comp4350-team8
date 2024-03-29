b5fcf767766398ef5bbc52a2d5e23dc88d41e60f
kienmai <maingockien01@gmail.com>
2024-03-28 21:35:54 -0500

    Refactor navbar

---
+++ apps/frontend/src/Components/NavbarLinks/links.ts
@@ -0,0 +1,8 @@
+export const links = [
+  {name: 'Home', path: '/home'},
+  {name: 'Courses Look Up', path: '/lookup'},
+  {name: 'Add/Drop Courses', path: '/add-drop'},
+  {name: 'Calendar', path: '/calendar'},
+  {name: 'Roadmap', path: '/roadmap'},
+  {name: 'Personal roadmap', path: '/roadmap/personal'},
+];
