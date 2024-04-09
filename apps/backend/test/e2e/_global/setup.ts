485c7f1614b1e8eafec5a5764d2aea7088cd387e
kienmai <maingockien01@gmail.com>
2024-04-09 11:48:03 -0500

    Load env for test e2e

--- apps/backend/test/e2e/_global/setup.ts
+++ apps/backend/test/e2e/_global/setup.ts
@@ -1,8 +1,10 @@
 import {INestApplication} from '@nestjs/common';
 import {Test, TestingModule} from '@nestjs/testing';
 import {AppModule} from '../../../src/app.module';
+import {loadEnv} from '../../../src/config';
 
 export const makeApp = async (): Promise<INestApplication> => {
+  loadEnv();
   const module: TestingModule = await Test.createTestingModule({
     imports: [AppModule],
   }).compile();
